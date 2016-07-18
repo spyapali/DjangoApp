e4c010e37adb2b3aa52c64c5ffc6395f342c5f20
SHALINI PYAPALI <spyapali@SHALINIs-MacBook-Pro.local>
2016-07-17 17:37:10 -0700

    added templates directory.

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # /polls/5
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 ]
