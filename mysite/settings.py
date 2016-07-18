315694879a342f5883a06e10dfe5f89602347364
SHALINI PYAPALI <spyapali@SHALINIs-MacBook-Pro.local>
2016-07-18 00:49:33 -0700

    Added tests; Part 5.

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'USER': 'spyapali',
+        'PASSWORD': 'sainath1129',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
